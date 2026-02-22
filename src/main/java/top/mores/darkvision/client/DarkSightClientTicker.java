97aa256522ecf2bc44395571bedec40382cd4871
Do0oMores <do0omores@outlook.com>
2026-02-22 17:13:13 +0800

    FIX：事件引用问题

--- src/main/java/top/mores/darkvision/client/DarkSightClientTicker.java
+++ src/main/java/top/mores/darkvision/client/DarkSightClientTicker.java
@@ -1,7 +1,7 @@
 package top.mores.darkvision.client;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ClientTickEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import top.mores.darkvision.Darkvision;
@@ -11,8 +11,8 @@ public final class DarkSightClientTicker {
     private DarkSightClientTicker() {}
 
     @SubscribeEvent
-    public static void onClientTick(ClientTickEvent event) {
-        if (event.phase == ClientTickEvent.Phase.END) {
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
             DarkSightClientState.clientTick();
         }
     }
